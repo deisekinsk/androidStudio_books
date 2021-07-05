6f9bb038a5719d75f0b2f010586a12457735e846
deisesilva <kinsk@msn.com>
2021-07-05 18:50:20 -0300

    Create RecyclerView | Set Layout | Package views

--- app/src/main/java/com/example/androidstudio_books/MainActivity.java
+++ app/src/main/java/com/example/androidstudio_books/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidstudio_books;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //access to recyclerview and set to R. Create a variable for recyclerView.
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
+        //set layout
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //data and layout of list
+        //recyclerView.setAdapter();
+
     }
 }
