3fc39448b2536076aa093d89eac885e6b64cae8e
deisesilva <kinsk@msn.com>
2021-07-11 04:00:53 -0300

    Data of books and test on emulator device

--- app/src/main/java/com/example/androidstudio_books/views/MainActivity.java
+++ app/src/main/java/com/example/androidstudio_books/views/MainActivity.java
@@ -35,10 +35,23 @@ public class MainActivity extends AppCompatActivity {
         List<BooksClass> booksClassList = new ArrayList<>();
 
         BooksClass b1 = new BooksClass(1L, "Quem tem medo do feminismo negro?","Djamila Ribeiro","Companhia das Letras", true);
+        BooksClass b2 = new BooksClass(2L, "Insubmissas Lágrimas de Mulheres","Conceição Evaristo","Malê", false);
+        BooksClass b3 = new BooksClass(3L, "(Como eu Ensino) Escravidão no Brasil","Joel Rufino Dos Santos","Melhoramentos", false);
+        BooksClass b4 = new BooksClass(4L, "Heroínas negras basileiras em 15 cordéis","Jarid Arraes","Pólen", true);
+        BooksClass b5 = new BooksClass(5L, "Rastros de resistência: Histórias de luta e liberdade do povo negro","Ale Santos","Panda Books", false);
+        BooksClass b6 = new BooksClass(6L, "O perigo de uma história única","Chimamanda Ngozi Adichie","Companhia das Letras", true);
+        BooksClass b7 = new BooksClass(7L, "Meio Sol Amarelo","Chimamanda Ngozi Adichie","Companhia das Letras", true);
+        BooksClass b8 = new BooksClass(8L, "Eu sou Malala - A história da garota que defendeu o direito à educação e foi baleada pelo Talibã","Malala Yousafzai, Christina Lamb","Companhia das Letras", false);
+
 
         booksClassList.add(b1);
-
-
+        booksClassList.add(b2);
+        booksClassList.add(b3);
+        booksClassList.add(b4);
+        booksClassList.add(b5);
+        booksClassList.add(b6);
+        booksClassList.add(b7);
+        booksClassList.add(b8);
 
         BooksAdapter booksAdapter = new BooksAdapter(booksClassList, this);
         recyclerView.setAdapter(booksAdapter);
