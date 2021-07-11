8476366820af80ed952366b9a061cc16800323c6
deisesilva <kinsk@msn.com>
2021-07-11 02:30:28 -0300

    Create attributes and methods

--- app/src/main/java/com/example/androidstudio_books/adapter/BooksAdapter.java
+++ app/src/main/java/com/example/androidstudio_books/adapter/BooksAdapter.java
@@ -1,18 +1,53 @@
 package com.example.androidstudio_books.adapter;
 
+import android.content.Context;
 import android.media.Image;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androidstudio_books.R;
+import com.example.androidstudio_books.dominio.BooksClass;
+
+import java.util.List;
 
 public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BookHolder> {
+    //attributes
+    private List<BooksClass> books;
+    private Context context;
+
+
+    //constructor
+    public BooksAdapter (List<BooksClass>books, Context context){
+        this.books = books;
+        this.context = context;
+    }
+
+
+    //methods
+    @NonNull
+    @Override
+    public BookHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BooksAdapter.BookHolder holder, int position) {
+
+    }
+
+    //method for size of list
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
     //iteration book list with layout for wich line
 
-    
+
 
     public class BookHolder extends RecyclerView.ViewHolder{
         public TextView txtTitle;
