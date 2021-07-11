51fa57f87e4ee3401bbda9de76cf58212ab11eee
deisesilva <kinsk@msn.com>
2021-07-11 02:23:16 -0300

    Class Book Adapter | item-Book information

--- app/src/main/java/com/example/androidstudio_books/adapter/BooksAdapter.java
+++ app/src/main/java/com/example/androidstudio_books/adapter/BooksAdapter.java
@@ -1,5 +1,37 @@
 package com.example.androidstudio_books.adapter;
 
-public class BooksAdapter {
+import android.media.Image;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidstudio_books.R;
+
+public class BooksAdapter extends RecyclerView.Adapter<BooksAdapter.BookHolder> {
     //iteration book list with layout for wich line
+
+    
+
+    public class BookHolder extends RecyclerView.ViewHolder{
+        public TextView txtTitle;
+        public TextView txtAuthor;
+        public TextView txtEmploy;
+        public ImageView ic_book;
+
+    //class that get information for cards
+        public BookHolder (View view){
+            super(view);
+
+            txtTitle = view.findViewById(R.id.txtTitle);
+            txtAuthor = view.findViewById(R.id.txtAuthor);
+            txtEmploy = view.findViewById(R.id.txtEmploy);
+            ic_book = view.findViewById(R.id.ic_book);
+
+        }
+
+    }
+
+
 }
