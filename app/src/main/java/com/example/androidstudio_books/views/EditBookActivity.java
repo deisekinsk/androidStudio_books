767a7e1fae5f6809a464257501ef7879feb6a082
deisesilva <kinsk@msn.com>
2021-07-19 02:36:21 -0300

    activity edit book

---
+++ app/src/main/java/com/example/androidstudio_books/views/EditBookActivity.java
@@ -0,0 +1,23 @@
+package com.example.androidstudio_books.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidstudio_books.R;
+
+public class EditBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_book);
+    }
+
+    public void cancel(View view) {
+    }
+
+    public void process(View view) {
+    }
+}
