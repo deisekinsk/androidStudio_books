80d04b73c9f4735867e28916bbb29c3b63e23d13
deisesilva <kinsk@msn.com>
2021-07-18 23:21:17 -0300

    DBHelper Class

---
+++ app/src/main/java/com/example/androidstudio_books/data/DBHelper.java
@@ -0,0 +1,20 @@
+package com.example.androidstudio_books.data;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    //constructor
+
+    //methods
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
