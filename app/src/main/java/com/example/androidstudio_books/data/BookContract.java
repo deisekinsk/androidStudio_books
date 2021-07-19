1b0e12efa8d3213ae91c7146e4c79e66ce28aef1
deisesilva <kinsk@msn.com>
2021-07-18 23:11:52 -0300

    BookContract Class for call and conect with DB

---
+++ app/src/main/java/com/example/androidstudio_books/data/BookContract.java
@@ -0,0 +1,19 @@
+package com.example.androidstudio_books.data;
+
+public class BookContract {
+
+    //for a access to data in table
+    public static final String TABLE_NAME = "bookTable";
+
+    //inner Class
+    public static final class Columns{
+        public static final String _ID = "_id";
+        public static final String title = "title";
+        public static final String author = "author";
+        public static final String company = "company";
+        public static final String read = "read";
+
+
+    }
+
+}
