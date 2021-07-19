aeebc3dd836e76e10ac02837e48358b48d70bd11
deisesilva <kinsk@msn.com>
2021-07-19 00:35:58 -0300

    BookDAO implements Method DB List_part2

--- app/src/main/java/com/example/androidstudio_books/data/BookDAO.java
+++ app/src/main/java/com/example/androidstudio_books/data/BookDAO.java
@@ -1,6 +1,7 @@
 package com.example.androidstudio_books.data;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.androidstudio_books.dominio.BooksClass;
@@ -29,9 +30,48 @@ public class BookDAO {
 
     //method for return a list of db
     public List<BooksClass> list(){
-        
+        //array for columns
+        String[] columns = {
+                BookContract.Columns._ID,
+                BookContract.Columns.title,
+                BookContract.Columns.author,
+                BookContract.Columns.company,
+                BookContract.Columns.read
+        };
+
+
+        //query | create a *cursor to be able to scroll through the data
+        try(
+
+        Cursor cursorData = db.query(BookContract.TABLE_NAME,
+                columns,
+                null,
+                null,
+                null,
+                null,
+                BookContract.Columns.title)
+        ){
+            if(cursorData.moveToFirst()){
+
+                do{
+                    BooksClass bookObj = BookDAO.fromCursor(cursorData);
+                    
+
+                }while (cursorData.moveToNext())
+            }
+
+        }
 
     }
 
+    private static BooksClass fromCursor(Cursor cursorData){
+        Long id = cursorData.getLong(cursorData.getColumnIndex(BookContract.Columns._ID));
+        String title = cursorData.getString(cursorData.getColumnIndex(BookContract.Columns.title));
+        String author = cursorData.getString(cursorData.getColumnIndex(BookContract.Columns.author));
+        String company = cursorData.getString(cursorData.getColumnIndex(BookContract.Columns.company));
+        int read = cursorData.getInt(cursorData.getColumnIndex(BookContract.Columns.read));
+
+        return  new BooksClass(id, title, author, company, read);
+    }
 
 }
