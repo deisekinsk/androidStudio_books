11276e82302c9e979253fcba542acea3a2430ae5
deisesilva <kinsk@msn.com>
2021-07-19 00:16:28 -0300

    BookDAO implements Method DB List

--- app/src/main/java/com/example/androidstudio_books/data/BookDAO.java
+++ app/src/main/java/com/example/androidstudio_books/data/BookDAO.java
@@ -1,4 +1,37 @@
 package com.example.androidstudio_books.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.androidstudio_books.dominio.BooksClass;
+
+import java.util.List;
+
 public class BookDAO {
+
+    //variables
+    private SQLiteDatabase db;
+    private static  BookDAO instance;
+
+    private BookDAO (Context context){
+        DBHelper dbHelper = DBHelper.getInstance(context);
+        //access for DB
+        db = dbHelper.getWritableDatabase();
+    }
+
+    //method for return instance of DAO
+    public static  BookDAO getInstance(Context context){
+        if(instance == null) {
+            instance = new BookDAO(context.getApplicationContext());
+        }
+        return instance;
+    }
+
+    //method for return a list of db
+    public List<BooksClass> list(){
+        
+
+    }
+
+
 }
